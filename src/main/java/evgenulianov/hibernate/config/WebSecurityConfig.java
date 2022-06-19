9b8096329719467f5c3d08ce34a64cfdfc3831bb
EvgenUlianov <ulianov_ea@mail.ru>
2022-06-19 17:08:05 +0300

    method-security

--- src/main/java/evgenulianov/hibernate/config/WebSecurityConfig.java
+++ src/main/java/evgenulianov/hibernate/config/WebSecurityConfig.java
@@ -1,8 +1,14 @@
 package evgenulianov.hibernate.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,23 +16,24 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig  {
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+public class WebSecurityConfig extends GlobalMethodSecurityConfiguration {
     //WebSecurityConfigurerAdapter deprecated
     //https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
+    //https://www.baeldung.com/spring-security-method-security
+    //
 
-    private static final String ROLE_USER = "USER";
+    private static final String PERSONS_READ = "PERSONS_READ";
+    private static final String PERSONS_WRITE = "PERSONS_WRITE";
+    private static final String PERSONS_DELETE = "PERSONS_DELETE";
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        // is needed for Postman testing: Authorisation Type = Basic Auth
+
         http
                 .formLogin()
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-city").permitAll()
-                .and()
-                .authorizeRequests().antMatchers("/persons/save").hasRole(ROLE_USER)
-                .and()
-                .authorizeRequests().anyRequest().authenticated()//.hasRole(ROLE_USER)
                 .and()
                 .httpBasic()
                 .and().csrf().disable()
@@ -34,14 +41,16 @@ public class WebSecurityConfig  {
         return http.build();
     }
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password("{noop}password")
-                .roles(ROLE_USER)
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    @Autowired
+    public void registerGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("reader").password("{noop}1").roles(PERSONS_READ)
+                .and()
+                .withUser("writer").password("{noop}1").roles(PERSONS_WRITE)
+                .and()
+                .withUser("deleter").password("{noop}1").roles(PERSONS_DELETE)
+        ;
     }
 
 }
--- src/main/java/evgenulianov/hibernate/controllers/PersonsController.java
+++ src/main/java/evgenulianov/hibernate/controllers/PersonsController.java
@@ -5,8 +5,11 @@ import evgenulianov.hibernate.model.Persons;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,27 +21,32 @@ public class PersonsController {
     private final PersonsService service;
 
     @GetMapping("/by-city")
+    @RolesAllowed({"PERSONS_READ"})
     public List<Persons> getPersonsByCity(@RequestParam String city) {
         return service.getPersonsByCity(city);
     }
 
     @PostMapping("/save")
+    @RolesAllowed({"PERSONS_WRITE"})
     public Persons save(@RequestBody Persons persons){
         return service.save(persons);
     }
 
     @PatchMapping ("/save")
+    @RolesAllowed({"PERSONS_WRITE"})
     public Persons update(@RequestBody Persons persons){
         return service.save(persons);
     }
 
 
     @GetMapping("/by-age")
+    @RolesAllowed({"PERSONS_READ"})
     public List<Persons> findByPersonIdAgeLessThanOrderByPersonIdAge(@RequestParam(name = "age") int age) {
         return service.findByPersonIdAgeLessThanOrderByPersonIdAge(age);
     }
 
     @GetMapping("/by-fullName")
+    @RolesAllowed({"PERSONS_READ"})
     public List<Persons> findByPersonIdNameAndAndPersonIdSurname(
             @RequestParam(name = "name") String personIdName,
             @RequestParam(name = "surname") String personIdSurname) {
@@ -46,6 +54,7 @@ public class PersonsController {
     }
 
     @GetMapping("/by-fullNameAge")
+    @RolesAllowed({"PERSONS_READ"})
     public ResponseEntity<Persons> findByPersonIdNameAndAndPersonIdSurname(
             @RequestParam(name = "name") String personIdName,
             @RequestParam(name = "surname") String personIdSurname,
@@ -58,6 +67,7 @@ public class PersonsController {
     }
 
     @DeleteMapping("/delete")
+    @RolesAllowed({"PERSONS_DELETE"})
     public void delete(@RequestBody Persons persons){
         service.delete(persons);
     }
